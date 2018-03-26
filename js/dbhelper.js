cfd249c3c83c8f9c331319dcf41b4eb387d9c411
danieldid <daniel.didusch@gmail.com>
2018-03-26 22:45:44 +0200

    Added Service Worker functionality

---
+++ js/dbhelper.js
@@ -0,0 +1,197 @@
+/**
+ * Common database helper functions.
+ */
+class DBHelper {
+
+    /**
+     * Database URL.
+     * Change this to restaurants.json file location on your server.
+     */
+    static get DATABASE_URL() {
+        const port = 8887 // Change this to your server port
+        return `data/restaurants.json`; //http://localhost:${port}/
+    }
+
+    /**
+     * Fetch all restaurants.
+     */
+    static fetchRestaurants(callback) {
+        caches.open('restaurant-v1').then(cache => {
+            cache.match('data/restaurants.json').then(response => {
+                const newResponse = response.clone();
+                
+                // when offline
+                if (typeof response !== undefined) {
+                    response = fetch(DBHelper.DATABASE_URL, {
+                        mode: 'no-cors'
+                    }).then(res => {
+                        return res.clone();
+                    });
+                }
+                
+                console.log(response);
+
+                // returning json
+                response.then(responseText => {
+                    responseText.text().then(text => {
+                        callback(null, JSON.parse(text).restaurants);
+                    }).catch(err => {
+                        console.log('Error encountered in text:');
+                        console.error(err);
+                    });
+                }).catch(err => {
+                    console.log('Error encountered in cache:');
+                    console.error(err);
+                });
+            }).catch(err => {
+                console.log('Error encountered in caches:');
+                console.error(err);
+            });
+        });
+    }
+
+    /**
+     * Fetch a restaurant by its ID.
+     */
+    static fetchRestaurantById(id, callback) {
+        // fetch all restaurants with proper error handling.
+        DBHelper.fetchRestaurants((error, restaurants) => {
+            if (error) {
+                callback(error, null);
+            } else {
+                const restaurant = restaurants.find(r => r.id == id);
+                if (restaurant) { // Got the restaurant
+                    callback(null, restaurant);
+                } else { // Restaurant does not exist in the database
+                    callback('Restaurant does not exist', null);
+                }
+            }
+        });
+    }
+
+    /**
+     * Fetch restaurants by a cuisine type with proper error handling.
+     */
+    static fetchRestaurantByCuisine(cuisine, callback) {
+        // Fetch all restaurants  with proper error handling
+        DBHelper.fetchRestaurants((error, restaurants) => {
+            if (error) {
+                callback(error, null);
+            } else {
+                // Filter restaurants to have only given cuisine type
+                const results = restaurants.filter(r => r.cuisine_type == cuisine);
+                callback(null, results);
+            }
+        });
+    }
+
+    /**
+     * Fetch restaurants by a neighborhood with proper error handling.
+     */
+    static fetchRestaurantByNeighborhood(neighborhood, callback) {
+        // Fetch all restaurants
+        DBHelper.fetchRestaurants((error, restaurants) => {
+            console.log('fetchRestaurantByNeighborhood');
+            if (error) {
+                console.error(error);
+                callback(error, null);
+            } else {
+                // Filter restaurants to have only given neighborhood
+                console.log(restaurants);
+                const results = restaurants.filter(r => r.neighborhood == neighborhood);
+                callback(null, results);
+            }
+        });
+    }
+
+    /**
+     * Fetch restaurants by a cuisine and a neighborhood with proper error handling.
+     */
+    static fetchRestaurantByCuisineAndNeighborhood(cuisine, neighborhood, callback) {
+        // Fetch all restaurants
+        DBHelper.fetchRestaurants((error, restaurants) => {
+            if (error) {
+                callback(error, null);
+            } else {
+                let results = restaurants
+                if (cuisine != 'all') { // filter by cuisine
+                    results = results.filter(r => r.cuisine_type == cuisine);
+                }
+                if (neighborhood != 'all') { // filter by neighborhood
+                    results = results.filter(r => r.neighborhood == neighborhood);
+                }
+                callback(null, results);
+            }
+        });
+    }
+
+    /**
+     * Fetch all neighborhoods with proper error handling.
+     */
+    static fetchNeighborhoods(callback) {
+        // Fetch all restaurants
+        DBHelper.fetchRestaurants((error, restaurants) => {
+            if (error) {
+                callback(error, null);
+            } else {
+                // Get all neighborhoods from all restaurants
+                const neighborhoods = restaurants.map((v, i) => restaurants[i].neighborhood)
+                // Remove duplicates from neighborhoods
+                const uniqueNeighborhoods = neighborhoods.filter((v, i) => neighborhoods.indexOf(v) == i)
+                callback(null, uniqueNeighborhoods);
+            }
+        });
+    }
+
+    /**
+     * Fetch all cuisines with proper error handling.
+     */
+    static fetchCuisines(callback) {
+        // Fetch all restaurants
+        DBHelper.fetchRestaurants((error, restaurants) => {
+            if (error) {
+                callback(error, null);
+            } else {
+                // Get all cuisines from all restaurants
+                const cuisines = restaurants.map((v, i) => restaurants[i].cuisine_type)
+                // Remove duplicates from cuisines
+                const uniqueCuisines = cuisines.filter((v, i) => cuisines.indexOf(v) == i)
+                callback(null, uniqueCuisines);
+            }
+        });
+    }
+
+    /**
+     * Restaurant page URL.
+     */
+    static urlForRestaurant(restaurant) {
+        return (`./restaurant.html?id=${restaurant.id}`);
+    }
+
+    /**
+     * Restaurant image URL.
+     */
+    static imageUrlForRestaurant(restaurant) {
+        return {
+            original: (`/img/${restaurant.photograph}`),
+            medium: (`/img/${restaurant.photograph_medium}`),
+            small: (`/img/${restaurant.photograph_small}`)
+        };
+        // return (`/img/${restaurant.photograph}`);
+    }
+
+    /**
+     * Map marker for a restaurant.
+     */
+    static mapMarkerForRestaurant(restaurant, map) {
+        const marker = new google.maps.Marker({
+            position: restaurant.latlng,
+            title: restaurant.name,
+            url: DBHelper.urlForRestaurant(restaurant),
+            map: map,
+            animation: google.maps.Animation.DROP
+        });
+        return marker;
+    }
+
+}
---
+++ sw.js
@@ -0,0 +1,127 @@
+const CACHE_NAME = 'restaurant-v1';
+const IMG_CACHE = 'restaurant-image-v1';
+const urlsToCache = [
+    '/',
+    '/css/styles.css',
+    '/js/main.js',
+    '/js/dbhelper.js',
+    '/js/restaurant_info.js',
+    '/data/restaurants.json'
+];
+
+
+self.addEventListener('install', install => {
+    console.log('serviceWorker installed!');
+    install.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Cache opened!');
+            return cache.addAll(urlsToCache).then(() => {
+                return self.skipWaiting(); // To forces the waiting service worker to become the active service worker
+            });
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    const requestUrl = new URL(event.request.url);
+
+    if (requestUrl.origin === location.origin) {
+        if (requestUrl.pathname === '/') {
+            event.respondWith(caches.match('/'));
+            return;
+        }
+        // cache and serve local images
+        if (requestUrl.pathname.startsWith('/img/')) {
+            event.respondWith(servePhoto(event.request));
+            return;
+        }
+        // cache and serve GMaps images
+        /*if (requestUrl.pathname.startsWith('/mapfiles/api-v3/images/')) {
+            event.respondWith(servePhoto(event.request));
+            return;
+        }*/
+    }
+
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            // const myResponse = response.clone();
+            return response || fetch(event.request, {
+                mode: 'no-cors'
+            });
+        })
+    );
+
+
+    /*event.respondWith(
+        caches.match(event.request)
+        .then(response => {
+            // Cache hit - return response
+            if (response) {
+                console.log('cached content delivered ' + event.request.url);
+                return response;
+            }
+
+            // IMPORTANT: Clone the request. A request is a stream and
+            // can only be consumed once. Since we are consuming this
+            // once by cache and once by the browser for fetch, we need
+            // to clone the response.
+            var fetchRequest = event.request.clone();
+
+            return fetch(fetchRequest).then(
+                response => {
+                    // Check if we received a valid response
+                    if(!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+
+                    // IMPORTANT: Clone the response. A response is a stream
+                    // and because we want the browser to consume the response
+                    // as well as the cache consuming the response, we need
+                    // to clone it so we have two streams.
+                    var responseToCache = response.clone();
+
+                    caches.open(CACHE_NAME)
+                        .then(cache => {
+                        cache.put(event.request, responseToCache);
+                    });
+
+                    return response;
+                }
+            );
+        }).catch(err => {
+            console.error(err);
+        })
+    );*/
+});
+
+function servePhoto(request) {
+    const storageUrl = request.url;
+
+    console.log('servePhoto ' + storageUrl);
+    
+    return caches.open(IMG_CACHE).then(cache => {
+        return cache.match(storageUrl).then(response => {
+            if (response) return response;
+
+            return fetch(request).then(networkResponse => {
+                cache.put(storageUrl, networkResponse.clone());
+                return networkResponse;
+            });
+        });
+    });
+}
+
+self.addEventListener('activate', (event) => {
+    console.info('Event: Activate');
+
+    // Remove old and unwanted caches
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) return caches.delete(cache);
+                })
+            );
+        })
+    );
+});
